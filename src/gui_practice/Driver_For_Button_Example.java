cb9143c2da1520f1342de954aa0e5d3db45c8252
asv520 <iamasv520@gmail.com>
2021-10-29 22:17:52 +0530

    Add 'Button' code

---
+++ src/gui_practice/Driver_For_Button_Example.java
@@ -0,0 +1,10 @@
+package gui_practice;
+
+public class Driver_For_Button_Example {
+        public static void main(String[] args) {
+
+            // JButton = a button that performs an action when clicked on
+
+            new Button_Example();
+        }
+}
